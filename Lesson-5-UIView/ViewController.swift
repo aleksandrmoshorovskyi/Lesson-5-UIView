e5259ab22cc17c62c4105bfd6c833b52dfb97ec8
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-03-04 20:45:24 +0200

    views

--- Lesson-5-UIView/ViewController.swift
+++ Lesson-5-UIView/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var yellowView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let someView = UIView()
+//        var someViewFrame = CGRect(x: 0, y: 0, width: 200, height: 200)
+//        someView.frame = someViewFrame
+//
+//        someViewFrame = someView.frame
+//        someViewFrame.origin.y += 20
+//        someView.frame = someViewFrame
+ 
+        /*
+        var yellowViewFrame = yellowView.frame
+        yellowViewFrame.origin.y += 400
+        
+        yellowView.frame = yellowViewFrame
+        */
     }
 
 
